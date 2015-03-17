7403fce680fb0ec1e43fa2f64e6e24878fd0a1ac
Di Ruan <ruandi88@gmail.com>
2015-03-17 00:02:22 -0400

    find the entity_type

--- Analyser.py
+++ Analyser.py
@@ -1,4 +1,5 @@
 import Search
+import re
 
 entity_types = {'/people/person': 'Person',
                 '/book/author': 'Author',
@@ -18,11 +19,26 @@ interest = {'Person': ['Name', 'Birthday'],
             'SportsTeam': ['Name', 'Description']}
 
 
-def build_infobox(self, results):
+def build_infobox(results):
     mids = []
     if results is not None:
         for result in results['result']:
             mids.append(result['mid'])
     for mid in mids:
+        count = 0
+        res_list = []
         topic = Search.get_search_result(mid)
-        topic['property']
+        for p in topic['property']:
+            entity_type = get_prefix(p)
+            if entity_types[entity_type] is not None:
+                count += 1
+                for value in topic['property'][entity_type]['values']:
+                    res_list.append([entity_types[entity_type], value['text']])
+        if count > 0:
+            return res_list
+    return []
+
+
+def get_prefix(entity_type):
+    res = re.search('/[a-z_]+/[a-z_]+', entity_type)
+    return res.group(0)
