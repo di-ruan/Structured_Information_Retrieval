0c157f3a3ca0788052eac6a7f702fb13315d3fc7
Jie-Gang Kuang <jk3735@columbia.edu>
2015-03-20 18:23:07 -0400

    Mark out test

--- Display.py
+++ Display.py
@@ -18,20 +18,20 @@ W_LIST = 50
 # Case List: display en element in a line
 # Case Dictionary: display the keys in the first line and values in the second with | in between
 # For example, try to parse this list
-
-test_list = [[1, 'Bill Gates(AUTHOR, BUSINESS_PERSON)'],
-             [2, 'Name', 'Bill Gates'],
-             [2, 'Description', 'William Henry "Bill" Gates III is an American business magnate, philanthropist,\
-                                investor, computer programmer, and inventor. Gates originally established his \
-                                reputation as the co-founder of Microsoft'],
-             [3, 'Siblings', ['Libby Gates', 'Kristi Gates']],
-             [4, 'Leadership', ['Organization', ['Microsoft Corporation', '', ''], 'Role',
-                                ['Chief Executive', '', 'CTO'], 'Title', ['Chief Executive', 'COO', 'CFO'],
-                                'From-To', ['', '', '1975-04-04 - now']]]]
-
-answer_list = [['Bill Gates', 'Business Person', ['Microsoft Corporation', 'Microsoft Research']],
-               ['Di Ruan', 'Author', ['Amazon']],
-               ['Jie-gang Kuang', 'Business Person', ['VMWare', 'Computer Engineering']]]
+#
+# test_list = [[1, 'Bill Gates(AUTHOR, BUSINESS_PERSON)'],
+#              [2, 'Name', 'Bill Gates'],
+#              [2, 'Description', 'William Henry "Bill" Gates III is an American business magnate, philanthropist,\
+#                                  investor, computer programmer, and inventor. Gates originally established his \
+#                                  reputation as the co-founder of Microsoft'],
+#              [3, 'Siblings', ['Libby Gates', 'Kristi Gates']],
+#              [4, 'Leadership', ['Organization', ['Microsoft Corporation', '', ''], 'Role',
+#                                ['Chief Executive', '', 'CTO'], 'Title', ['Chief Executive', 'COO', 'CFO'],
+#                                'From-To', ['', '', '1975-04-04 - now']]]]
+#
+# answer_list = [['Bill Gates', 'Business Person', ['Microsoft Corporation', 'Microsoft Research']],
+#                ['Di Ruan', 'Author', ['Amazon']],
+#                ['Jie-gang Kuang', 'Business Person', ['VMWare', 'Computer Engineering']]]
 
 def padding_space(strlist, target_size):
     if target_size > len(strlist):
